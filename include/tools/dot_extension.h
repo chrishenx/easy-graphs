ab3b13b74ba78b23c9aec72b2c2273a0185f59f2
ChrisHenx <chrishendrix24@gmail.com>
2016-03-14 12:42:49 -0600

    Two simple functions to call the dot engine from code using system calls.

---
+++ include/tools/dot_extension.h
@@ -0,0 +1,48 @@
+/*
+  Implementation of utility functions which make a system call
+  to graphiz in order to generate image representations of graphs
+  using their dot-representation.
+  
+  @author Christian González León
+ */
+
+#ifndef DOT_EXTENCION_H
+#define DOT_EXTENCION_H
+
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+inline static bool dotRepr2PNG(const std::string& dot_repr,
+    const std::string& filename, const std::string& folder = "") {
+  std::ofstream ofs("graph.gv");
+  if (ofs.fail()) {
+    ofs.close();
+    return false;
+  }
+  ofs << dot_repr;
+  ofs.close();
+  system(std::string("/usr/local/bin/dot -Tpng graph.gv -o " + filename + ".png").c_str());
+  if (!folder.empty()) {
+    system(std::string("move " + filename + ".png " + folder).c_str());
+  }
+  return true;
+}
+
+inline static bool dotRepr2JPEG(const std::string& dot_repr,
+    const std::string& filename, const std::string& folder = "") {
+  std::ofstream ofs("graph.gv");
+  if (ofs.fail()) {
+    ofs.close();
+    return false;
+  }
+  ofs << dot_repr;
+  ofs.close();
+  system(std::string("/usr/local/bin/dot -Tjpeg graph.gv -o " + filename + ".jpg").c_str());
+  if (!folder.empty()) {
+    system(std::string("move " + filename + ".png " + folder).c_str());
+  }
+  return true;
+}
+
+#endif // DOT_EXTENCION_H
