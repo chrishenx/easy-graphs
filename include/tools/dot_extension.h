8e838eecd9baf51675db7a03ce24e6523e44c529
ChrisHenx <chrishendrix24@gmail.com>
2016-03-14 14:04:00 -0600

    functions from dot_extension.h wrapped within a struct

--- include/tools/dot_extension.h
+++ include/tools/dot_extension.h
@@ -13,38 +13,42 @@
 #include <fstream>
 #include <cstdlib>
 
-static const std::string dotPath = "dot"; // asuming that is in the system path  
+struct dot_extension {
+  static std::string dotPath = "dot"; // asuming that is in the system path  
 
-inline static bool dotRepr2PNG(const std::string& dot_repr,
-    const std::string& filename, const std::string& folder = "") {
-  std::ofstream ofs("graph.gv");
-  if (ofs.fail()) {
+  inline static bool dotRepr2PNG(const std::string& dot_repr,
+      const std::string& filename, const std::string& folder = "") {
+    std::ofstream ofs("graph.gv");
+    if (ofs.fail()) {
+      ofs.close();
+      return false;
+    }
+    ofs << dot_repr;
     ofs.close();
-    return false;
+    system((dotPath + " -Tpng graph.gv -o " + filename + ".png").c_str());
+    if (!folder.empty()) {
+      system(std::string("move " + filename + ".png " + folder).c_str());
+    }
+    return true;
   }
-  ofs << dot_repr;
-  ofs.close();
-  system((dotPath + " -Tpng graph.gv -o " + filename + ".png").c_str());
-  if (!folder.empty()) {
-    system(std::string("move " + filename + ".png " + folder).c_str());
-  }
-  return true;
-}
 
-inline static bool dotRepr2JPEG(const std::string& dot_repr,
-    const std::string& filename, const std::string& folder = "") {
-  std::ofstream ofs("graph.gv");
-  if (ofs.fail()) {
+  inline static bool dotRepr2JPEG(const std::string& dot_repr,
+      const std::string& filename, const std::string& folder = "") {
+    std::ofstream ofs("graph.gv");
+    if (ofs.fail()) {
+      ofs.close();
+      return false;
+    }
+    ofs << dot_repr;
     ofs.close();
-    return false;
-  }
-  ofs << dot_repr;
-  ofs.close();
-  system((dotPath + " -Tjpeg graph.gv -o " + filename + ".jpg").c_str());
-  if (!folder.empty()) {
-    system(std::string("move " + filename + ".png " + folder).c_str());
-  }
-  return true;
+    system((dotPath + " -Tjpeg graph.gv -o " + filename + ".jpg").c_str());
+    if (!folder.empty()) {
+      system(std::string("move " + filename + ".png " + folder).c_str());
+    }
+    return true;
+  }  
+  
+  
 }
 
 #endif // DOT_EXTENCION_H
